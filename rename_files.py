84c4047b4a42e4482b87f3b83fc132d77aa0e7d7
yurys <yury.salyatov@gmail.com>
2026-08-25 19:12:09 +0300

    add normalizetion for embs

--- rename_files.py
+++ rename_files.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import os
 import argparse
 
@@ -21,6 +22,28 @@ for name in names:
     filename = "homo_graph_with_embeddings.pt"
 
     homo_data = torch.load(f"{graph_dir}{name}/{filename}", weights_only=False)
+    print(homo_data["node_text_feat"][0])
+    # Нормализация эмбеддингов узлов
+    if "node_text_feat" in homo_data:
+        if isinstance(homo_data["node_text_feat"], list):
+            for i, n_emb in enumerate(homo_data["node_text_feat"]):
+                homo_data["node_text_feat"][i] = F.normalize(n_emb, p=2, dim=-1)
+            homo_data["node_text_feat"] = torch.tensor(homo_data["node_text_feat"], dtype=torch.float32)
+        else:
+            homo_data["node_text_feat"] = F.normalize(homo_data["node_text_feat"], p=2, dim=-1)
+    print(homo_data["node_text_feat"][0])
+
+    print(homo_data["edge_text_feat"][0])
+    # Нормализация эмбеддингов рёбер
+    if "edge_text_feat" in homo_data:
+        if isinstance(homo_data["edge_text_feat"], list):
+            for i, edge_emb in enumerate(homo_data["edge_text_feat"]):
+                homo_data["edge_text_feat"][i] = F.normalize(edge_emb, p=2, dim=-1)
+            homo_data["edge_text_feat"] = torch.tensor(homo_data["node_text_feat"], dtype=torch.float32)
+        else:
+            homo_data["edge_text_feat"] = F.normalize(homo_data["edge_text_feat"], p=2, dim=-1)
+    print(homo_data["edge_text_feat"][0])
+
     save_path = f"data/{name}/processed"
     os.makedirs(save_path, exist_ok=True)
     torch.save(homo_data, f"{save_path}/geometric_data_processed.pt")
